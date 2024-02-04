0e5a1a9daad5a381f45497944fc54a432c2b95e6
Aditya <113250246+Aditya-Pande374@users.noreply.github.com>
2024-02-04 20:07:30 +0530

    Updated path for music

--- script.js
+++ script.js
@@ -9,14 +9,14 @@ const audioTrack=[
         number: 1,
         name: 'Teri Deewani',
         artist: 'Kailash Kher',
-        src: '/super-duper-oct-system/groove/Teri Deewani - Kailash Kher _ Official Video _ Kailasa _ Paresh _ Naresh.mp3',
+        src: 'groove/Teri Deewani - Kailash Kher _ Official Video _ Kailasa _ Paresh _ Naresh.mp3',
         image: "https://c.saavncdn.com/789/Kailasa-Kailash-Kher-Hindi-2006-500x500.jpg"
     },
     {
         number: 2,
         name: 'Fear&Loathing',
         artist: 'Marina',
-        src: '/super-duper-oct-system/groove/MARINA AND THE DIAMONDS - FEAR & LOATHING _Official Music Video_ _ ♡ ELECTRA HEART PART 1_11 ♡ ( 160kbps ).mp3',
+        src: 'groove/MARINA AND THE DIAMONDS - FEAR & LOATHING _Official Music Video_ _ ♡ ELECTRA HEART PART 1_11 ♡ ( 160kbps ).mp3',
         image: "https://static.wikia.nocookie.net/marina/images/8/88/Tumblr_lpp8fr8UWN1qf250ao1_500.png/revision/latest/scale-to-width-down/250?cb=20190302204313"
 
     },
@@ -24,42 +24,42 @@ const audioTrack=[
         number: 3,
         name: 'Dynasty',
         artist: 'MIIA',
-        src: '/super-duper-oct-system/groove/MIIA - Dynasty (Lyrics).mp3',
+        src: 'groove/MIIA - Dynasty (Lyrics).mp3',
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB227fkdqB2S8hRB1QESVkkq1Xc0NCbJOvCZ5Bsce9daOJQelBmixy2zS0GHf_2iPPonI&usqp=CAU"
     },
     {
         number: 4,
         name: 'Boy Like You',
         artist: 'NightCore',
-        src: '/super-duper-oct-system/groove/Nightcore - Boy Like You (Lyrics) ( 160kbps ).mp3',
+        src: 'groove/Nightcore - Boy Like You (Lyrics) ( 160kbps ).mp3',
         image: "https://e1.pxfuel.com/desktop-wallpaper/386/1022/desktop-wallpaper-nightcore-anime-nightcore.jpg"
     },
     {
         number: 5,
         name: 'La Da Dee',
         artist: 'NightCore',
-        src: '/super-duper-oct-system/groove/Nightcore - La Da Dee ( 128kbps ).mp3',
+        src: 'groove/Nightcore - La Da Dee ( 128kbps ).mp3',
         image: "https://c4.wallpaperflare.com/wallpaper/181/330/398/nightcore-angel-with-a-shotgun-wallpaper-preview.jpg"
     },
     {
         number: 6,
         name: 'La La La',
         artist: 'NightCore',
-        src: '/super-duper-oct-system/groove/Nightcore - La La La ( 256kbps cbr ).mp3',
+        src: 'groove/Nightcore - La La La ( 256kbps cbr ).mp3',
         image: "https://i.ytimg.com/vi/KdVmKAzZBQg/maxresdefault.jpg"
     },
     {
         number: 7,
         name: 'Ek Main Aur Ekk Tu',
         artist: 'Benny Dayal',
-        src: '/super-duper-oct-system/groove/yt1s.com - Ek Main Aur Ekk Tu Title Track lyrical Video  Benny Dayal Anushka  Imran Khan  Kareena Kapoor.mp3',
+        src: 'groove/yt1s.com - Ek Main Aur Ekk Tu Title Track lyrical Video  Benny Dayal Anushka  Imran Khan  Kareena Kapoor.mp3',
         image: "https://c.saavncdn.com/686/Ek-Main-Aur-Ekk-Tu-2011-500x500.jpg"
     },
     {
         number: 8,
         name: 'Mirrors',
         artist: 'Justin Timberlake',
-        src: '/super-duper-oct-system/groove/yt1s.com - Justin Timberlake  Mirrors.mp3',
+        src: 'groove/yt1s.com - Justin Timberlake  Mirrors.mp3',
         image: "https://m.media-amazon.com/images/M/MV5BZTJjNDA0YzUtZDVmZS00M2JmLThiNzAtYjAyMWEwZjM0MDdmL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNDQ5MDYzMTk@._V1_.jpg"
     }
 ]
